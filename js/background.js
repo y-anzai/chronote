cba62edcbef04bea5c21847d0bb1b3d8871b6de7
Yujiro Anzai <qq10qqq@gmail.com>
2020-07-26 23:59:18 +0900

    カウント機能追加

--- js/background.js
+++ js/background.js
@@ -7,19 +7,45 @@
     "title": "bookmark"
   });
 
+  var local = chrome.storage.local;
+  var syncs = chrome.storage.sync;
+
+	var setStorage = function(key,val){
+		local.set({key: JSON.stringify(val)});
+	};
+
+  // param array
+  var getStorage = function(keys){
+    local.get(keys, function(obj){
+      return JSON.parse(obj);
+    });
+  };
+
+
+  // local.get(["key1", "key2", "key3"], function (value) {
+  //   var value_data_1 = value.key1;
+  //   var value_data_2 = value.key2;
+  //   var value_data_3 = value.key3;
+  // });
+　
+
   // タブを開いたときに
   var url;
 
   // タブ切り替えた時
   chrome.tabs.onActivated.addListener(function(tabId) {
-      createURL();
+    console.log('切り替えた');
+    createURL();
   });
   // タブ読み込み時
-  chrome.tabs.onUpdated.addListener(function(tabId, opt) {
+  chrome.tabs.onUpdated.addListener(function(tabId, opt) {    
+      console.log('タブ読み込み');
       createURL();
   });
   // タブ作成時
   chrome.tabs.onCreated.addListener(function(tab){
+
+    console.log('タブ作成時');
       createURL();
   });
   // 右上のアイコン押下時
@@ -40,14 +66,40 @@
 
         // "http://"を削除、"https://"の時は"s/"に置換
         url = tabs[0].url.replace(/^(http:\/\/)/,"");
-        console.log(tabs);
+        // local.set({'urls': url}, function () {
+        // });
+        
         if(url === tabs[0].url){
             url = url.replace(/^(https:\/\/)/,"s/");
         }
+        URLsaveChanges(url);
     });
-}
+  }
 
+  function URLsaveChanges(url) {
+    var map, count;
+    local.get(['urls', 'count'], function(result){
+      console.log(`urls: ${result.urls} count: ${result.count}`);
+      //urls = isValidJson(r.urls) ? JSON.parse(r.urls) : [];
+      console.log(`r.urls: ${result.urls}`);
+      map = result.urls ? JSON.parse(result.urls) : {};
 
+      // オブジェクトを配列変換: if (map instanceof Object) {map = Object.keys(map).map(function (key) {return map[key]})} 
+      if (typeof map[url] == 'object') {
+        map[url].count++;
+      } else {
+        map[url] = {'count' : 1};
+      }
+      map[url].updated = new Date();
+      // TODO: map[url].activeTime
+      //map[url].count = (map[url].count +1) || 1;
+      count = result.count? parseInt(result.count): 0;
+      count++; 
+      local.set({'urls': JSON.stringify(map),'count': count}, function() {
+        
+      });
+    });
+  }
 
   // ブックマークのデータ構造を取得する
 
